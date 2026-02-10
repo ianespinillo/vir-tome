4f65a149323df78b80320318f1ff8fe658afbc60
Ian Espinillo <ianespinillo@gmail.com>
2026-02-10 16:07:49 -0300

    fix(api):fixed core types

--- apps/api/src/core/core.types.ts
+++ apps/api/src/core/core.types.ts
@@ -1,14 +1,12 @@
-import { PAYLOAD_TYPE, ROLES } from '@repo/common';
-import { TenantEntity } from '../tenants/entities/tenant.entity';
-import { UserEntity } from '../users/entities/user.entity';
+import { ITenant, IUser, PAYLOAD_TYPE, ROLES } from '@repo/common';
 
 // Definimos la estructura exacta que retorna tu JwtStrategy
 export interface IAuthUser {
 	id: number;
 	email: string;
 	type: PAYLOAD_TYPE;
-	entity: UserEntity; // Puedes reemplazar 'any' por SuperAdminEntity si la tienes importada
-	tenant?: TenantEntity;
+	entity: IUser; // Puedes reemplazar 'any' por SuperAdminEntity si la tienes importada
+	tenant?: ITenant;
 	tenantId: number;
 	roleId?: number;
 	roleName: ROLES;
@@ -19,7 +17,7 @@ export interface IAuthUser {
 declare module 'express' {
 	interface Request {
 		// Propiedades inyectadas por tu TenantMiddleware
-		tenant: TenantEntity;
+		tenant: ITenant;
 		tenantId: number;
 
 		// Propiedad inyectada por Passport (ahora fuertemente tipada)
